031ecef06239d2e7bd3c687672935bfdeca3241f
Vygantas Vilius <biesaz@gmail.com>
2023-10-11 23:53:39 +0300

    nihuja nesigauna

--- task1.py
+++ task1.py
@@ -8,6 +8,25 @@
 # For storing account information use dict , all data should be lowercase. and extra symbols (@, %, $ etc and numbers). 
 # If in password numbers or symbols are not provided, the program should add additional value to dictionary as a value, names 'improved password'
 
-username_password = input("Enter 5 usernames: ")
-base = {}
-patikrinti
+username_password = input("Enter 5 username= , password= ")
+base = []
+username = "baba"
+password = 123
+base.append(password)
+base.append(username)
+
+for data in base:
+    username, password = data.split('=')
+    base[username.strip()] = password.strip()
+attempts = 0
+
+while True:
+    username_password = input("Enter 5 username= , password= ")
+    if username_password in base and base[username_password] == password:
+        print("Welcome! Access granted.")
+        break
+    else:
+        print("Wrong credentials. Please try again.")
+        attempts += 1
+
+print(f"Number of attempts: {attempts}")
